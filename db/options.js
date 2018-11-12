2ee05eb98aa1a098820517df2ea1e4aeaac47aa6
amine <bouamama.amine@bcg.com>
2018-11-12 10:56:05 +0100

    user table

--- db/options.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-module.exports =  {
-  config: path.join(__dirname, '../config/config.json'),
-  'migrations-path': path.join(__dirname, 'sql/migrations'),
-  'seeders-path': path.join(__dirname, 'sql/seeders'),
-  'models-path': path.join(__dirname, 'sql/models'),
-};
