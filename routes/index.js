2a9fa394f09bbf5b509d13cab240a0d2ca4d7bce
amine <bouamama.amine@bcg.com>
2018-11-15 15:21:30 +0100

    post process-login

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router  = express.Router();
+const User    = require('../db/index').db.sql.User;
 
 
 router.get('/', (req, res, next) => {
@@ -12,7 +13,37 @@ router.get('/login', (req, res, next) =>{
 
 router.post('/process-login', (req, res, next) => {
 
-  res.send(req.body);
+  const {email, originalPassword} = req.body;
+
+  async function login(){
+
+    let user = await User.findOne({where: {email: email}});
+
+    if(user === null){
+      // user not found
+      req.flash('error', `User ${email} is not registered yes on Slack! SIgn Up and Enjoy all Slack functionnalities for free !! 🙌`);
+      res.redirect('/login');
+      return
+    }
+
+    // if user found
+
+    // check password
+    let isValidPassword = true; // TODO: FIXME
+
+    if(!isValidPassword){
+      req.flash('error', `Sorry ! 🤭 Wrong Password ! `);
+      res.redirect('/login');
+      return
+    }
+
+    req.logIn(user, () => {
+      req.flash("success", `Welcome back ${user.nickname} ! Happy to see you ! 😁`);
+      res.redirect('/');
+    });
+
+  }
+  login();
 
 });
 
