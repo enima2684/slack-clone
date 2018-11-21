d55d75648183933dc5ca4e1edfd05e42a7242a05
amine <bouamama.amine@bcg.com>
2018-11-21 16:15:19 +0100

    fix

--- db/sql/models/index.js
+++ db/sql/models/index.js
@@ -7,17 +7,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../../../config/config-sequelize.js')[env];
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  if(env === 'production'){
-    sequelize = new Sequelize(process.env.DATABASE_URL)
-  } else {
-    sequelize = new Sequelize(config.database, config.username, config.password, config);
-  }
-}
-
+let sequelize = new Sequelize(process.env.DATABASE_URL);
 
 
 /*** SCHEMAS IMPORT ***/
